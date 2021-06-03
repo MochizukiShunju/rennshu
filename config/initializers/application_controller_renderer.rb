d8a5014a50cd490da207f9588e2f91be808523eb
MochizukiShunju <b1302475@gmail.com>
2021-06-03 10:07:27 +0000

    gisou

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '1edf72cd2f474d0396276995705bf5c63a9dc7ca3a66495a979210f3b3461a3ec61a23b7b935c7ca05e3d27cdab7ac53e4a4b1f278f710c5460ed40597472760'
